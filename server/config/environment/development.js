b670781cef9ea63631d97e29f9daef1018e8dd54
Garrett <gfitzgerald123@gmail.com>
2015-06-12 11:55:12 -0500

    job list and map side by side

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,9 +6,9 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     // uri: 'mongodb://localhost/thesignup2'
-    uri: 'mongodb://dapperdingo:dapperpw@ds037812.mongolab.com:37812/legacy'
-    // uri: 'mongodb://localhost',  //#DD: Used for local hosting versus online MongoDB
-    // port: 27017									//#DD: Used for local hosting versus online MongoDB
+    // uri: 'mongodb://dapperdingo:dapperpw@ds037812.mongolab.com:37812/legacy'
+    uri: 'mongodb://localhost',  //#DD: Used for local hosting versus online MongoDB
+    port: 27017									//#DD: Used for local hosting versus online MongoDB
   },
 
   seedDB: false //#DD: Changing to false prevents DB from populating with premade data
