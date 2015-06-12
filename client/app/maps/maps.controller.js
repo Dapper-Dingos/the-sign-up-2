4dc92eef4ef6c074d0d899d415c805a3532bf091
Dan Corman <dan.corman@gmail.com>
2015-06-12 11:37:10 -0500

    Display boxes working styling

--- client/app/maps/maps.controller.js
+++ client/app/maps/maps.controller.js
@@ -2,17 +2,37 @@
 
 
 angular.module('theSignUp2App')
-  .controller('mapsController', function ($scope, $log, $timeout, JobsFactory) {
+.factory('GoogleMaps', function() {
+  var maps = {};
+
+  function addMap(mapId) {
+    maps[mapId] = {};
+  }
+  function getMap(mapId) {
+    if (!maps[mapId]) addMap(mapId);
+    return maps[mapId];
+  }
+
+  return {
+    addMap: addMap,
+    getMap: getMap
+  }
+})
+
+  .controller('mapsController', function ($scope, $log, $timeout, JobsFactory, GoogleMaps) {
     $scope.map = {center: {latitude: 30.2500, longitude: 97.7500}, zoom: 12 };
     $scope.options = {scrollwheel: false};
     $scope.coordsUpdates = 0;
     $scope.dynamicMoveCtr = 0;
     $scope.message = 'Its working!!!!';
     $scope.geocoder;
+     $scope.infowindow = new google.maps.InfoWindow();
     $scope.jobsList;
     $scope.marker = {
-      id: 0
-      }
+      id: 0,
+      show: false,
+      };
+
       
     //function for taking 
 	$scope.codeAddress = function() {
@@ -42,7 +62,14 @@ angular.module('theSignUp2App')
 
 
 
+	 $scope.windowOptions = {
+            show: false
+        };
 
+	 $scope.closeClick = function() {
+	 	console.log('closing')
+            $scope.windowOptions.show = !$scope.windowOptions.show;
+        };
 
 	$scope.markerJobs = function(){
 		JobsFactory.getJobs()
@@ -61,32 +88,11 @@ angular.module('theSignUp2App')
 	$scope.markerJobs();
 
 
-	var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
 
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  });
+	$scope.jobTracker = function(job){
+	}
 
 
 
 
-  });
+});
